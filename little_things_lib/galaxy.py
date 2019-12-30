fix radian conv

little_things_lib/galaxy.py
@@ -146,8 +146,8 @@ class Galaxy:
         self.v_systemic = v_systemic
         self.ring_parameters = {
             radius: {
-                'inc': inc * (np.pi/360),
-                'pos_ang': pos_ang * (np.pi/360),
+                'inc': inc * (np.pi/180),
+                'pos_ang': pos_ang * (np.pi/180),
                 'x_center': x,
                 'y_center': y
             }
@@ -155,8 +155,8 @@ class Galaxy:
                 in zip(radii, inclination, position_angle, x_pix_center, y_pix_center)
         }
         self.interp_ring_parameters = {
-            'inc': interp1d(radii, inclination),
-            'pos_ang': interp1d(radii, position_angle),
+            'inc': interp1d(radii, inclination * np.pi/180),
+            'pos_ang': interp1d(radii, position_angle * np.pi/180),
             'x_center': interp1d(radii, x_pix_center),
             'y_center': interp1d(radii, y_pix_center)
         }
@@ -193,7 +193,8 @@ class Galaxy:
         imputer = KNNImputer(n_neighbors=2, weights="uniform")
         v_field = imputer.fit_transform(v_field)
         v_field[v_field == 0] = np.nan
-
+        # rotate to match the fits data field
+        v_field = np.rot90(v_field, 3)
         return v_field
 
 
@@ -233,8 +234,8 @@ class Galaxy:
         #pos_ang = self.ring_parameters[r]['pos_ang']
         inc = self.interp_ring_parameters['inc'](r)
         pos_ang = self.interp_ring_parameters['pos_ang'](r)
-        x_kpc = r * (np.sin(2*pos_ang) * np.sin(theta) - np.cos(2*pos_ang) * np.cos(theta) * np.cos(inc))
-        y_kpc = r * (np.cos(2*pos_ang) * np.sin(theta) + np.sin(2*pos_ang) * np.cos(theta) * np.cos(inc))
+        x_kpc = r * (np.sin(pos_ang) * np.sin(theta) - np.cos(pos_ang) * np.cos(theta) * np.cos(inc))
+        y_kpc = r * (np.cos(pos_ang) * np.sin(theta) + np.sin(pos_ang) * np.cos(theta) * np.cos(inc))
         x_pix = x_kpc / self.kpc_per_pixel
         y_pix = y_kpc / self.kpc_per_pixel
         return (x_pix, y_pix)

little_things_lib/nfw_mcmc_fitter.py
@@ -28,7 +28,6 @@ def convert_to_mcmc_parameter_space(physical_space_parameters):
     return np.log10(theta_physical[0]), np.log10(theta_physical[1]), theta_physical[2]
 
 
-
 def get_mcmc_start_position(
         galaxy,
         number_grid_points_per_param=None
@@ -113,14 +112,14 @@ def lnlike(
         print('rhos, rs, v2_dm =',  rhos, rs, v2_dm)
 
     # TODO: probably have to interpolate radii and rotation here to make the 2d modeled field
-    chisq = chisq_2d(galaxy, galaxy.radii, v_m)
+    chisq, model_2d_field = chisq_2d(galaxy, galaxy.radii, v_m, record_array=True)
 
     return -0.5 * (chisq ), \
-           (rhos, rs, chisq, np.sqrt(v2_dm), np.sqrt(v2_baryons), v_d, v_m, dm_mass_enclosed)
+           (rhos, rs, chisq, np.sqrt(v2_dm), np.sqrt(v2_baryons),
+            v_d, v_m, dm_mass_enclosed, model_2d_field)
 
 
 def lnprior(theta, bounds):
-
     for item, bound in zip(theta, bounds):
         if not bound[0] <= item <= bound[1]:
             return -np.inf
@@ -147,7 +146,8 @@ def chisq_2d(
         radii_model,
         v_rot_1d_model,
         v_err_2d=None,
-        v_err_const=2.
+        v_err_const=2.,
+        record_array=False
 ):
     """
     :param v_rot_1d_model:
@@ -168,5 +168,8 @@ def chisq_2d(
         chisq = np.nansum((vlos_2d_data - vlos_2d_model) ** 2 / v_err_2d ** 2)
     else:
         chisq = np.nansum((vlos_2d_data - vlos_2d_model) ** 2 / v_err_const ** 2)
-    return chisq
+    if record_array:
+        return chisq, vlos_2d_model
+    else:
+        return chisq, None
 